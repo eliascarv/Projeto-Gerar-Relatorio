97e013a713c5ce540612c5ac3e88f4c9dc052fc4
Elias Carvalho <eliascarv1998@gmail.com>
2021-07-17 16:24:46 -0300

    Primeira Versão

--- gerar_resultado.py
+++ gerar_resultado.py
@@ -1,5 +1,5 @@
 from openpyxl import load_workbook
-from openpyxl.styles.borders import Border, Side
+from openpyxl.styles import Border, Side, PatternFill
 from statistics import mean, stdev, median
 from datetime import date
 
@@ -10,10 +10,27 @@ thin_border = Border(
     bottom = Side(style = 'thin')
 )
 
+def color_row(row_tuple, color):
+    background = PatternFill(start_color = color, fill_type = "solid")
+    for cell in row_tuple:
+        cell.fill = background
+
 wb = load_workbook('relatorio_painel.xlsx')
 ws = wb.active
 
 lastrow = ws.max_row
+
+deve_conter = ['CANETA', 'MARCA-TEXTO']
+pode_conter = ['P1', 'P2'] 
+proibidas = ['P3', 'P4']
+
+for row in ws.iter_rows(min_row = 6, max_row = lastrow, max_col = 12):
+    descr = row[4].value
+    if any(x in descr for x in proibidas):
+        continue
+
+    if all(x in descr for x in deve_conter) or (all(x in descr for x in deve_conter) and any(x in descr for x in pode_conter)):
+        color_row(row, 'e2f0d9')
 
 unit_values = [float(cell[0].value.replace(',', '.')) for cell in ws['H6':f'H{lastrow}']]
 meses = [int(cell[0].value.split('/')[1]) for cell in ws['L6':f'L{lastrow}']]
@@ -23,12 +40,18 @@ mes_atual = date.today().month
 ano_atual = date.today().year
 
 valores = []
-for mes, ano, i in zip(meses, anos, range(0, lastrow)):
-    if ano == 2020 and mes in range(mes_atual, 13):
-        valores.append(unit_values[i])
+for row, i in zip(ws.iter_rows(min_row = 6, max_row = lastrow, min_col = 12, max_col = 12), range(0, lastrow-5)):
+    cell = row[0]
+    data = cell.value
+    bgcolor = cell.fill.start_color.index
+    mes = int(data.split('/')[1])
+    ano = int(data.split('/')[2])
+    if bgcolor == '00e2f0d9':
+        if ano == 2020 and mes in range(mes_atual, 13):
+            valores.append(unit_values[i])
 
-    if ano == 2021 and mes in range(mes_atual + 1):
-        valores.append(unit_values[i])
+        elif ano == 2021 and mes in range(mes_atual + 1):
+            valores.append(unit_values[i])
         
 media = mean(valores)
 desvio = stdev(valores)
@@ -60,3 +83,8 @@ ws[f'B{lastrow + 5}'].border = thin_border
 ws[f'B{lastrow + 6}'].border = thin_border
 
 wb.save('resultado.xlsx')
+
+
+# row_tuple = ws[6]
+
+# color_row(row_tuple, 'e2f0d9')
